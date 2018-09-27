dbebefa74967f5feb39180ff918a754f84aa4e6e
sergeybandar <sergeybandar@mail.ru>
2018-09-27 19:37:36 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var i =0;
+var j=0;
+var a = str.split('');
+var b = a.length/2;
+for(var k=0; k<b;k++)
+{
+j=0;
+	for(var j=0;j<bracketsConfig.length;j++)
+	{
+		var temp = bracketsConfig[j];
+		i=0;
+		while(i<a.length)
+		{
+			if(a[i]==temp[0] && a[i+1]==temp[1])
+			{
+				a.splice(i,2);
+				i=0;
+			}
+			else i++;
+		}
+
+	}
+}
+
+if (a.length>0)
+{
+	return false;
+}
+else{
+	return true;
+}
+if (a.length>0)
+{
+	return false;
+}
+else{
+	return true;
+}
 }
